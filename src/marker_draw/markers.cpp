afd75396a0ab075c86c81b65ffb62b8155649330
Ben Kolligs <bkolligs@andrew.cmu.edu>
2021-04-10 14:43:02 -0400

    visualizer marker achieved

--- src/marker_draw/markers.cpp
+++ src/marker_draw/markers.cpp
@@ -6,75 +6,70 @@
 class MarkerNode {
 
     ros::Publisher markerPub_;
-    ros::NodeHandle nodeHandler_;
+    ros::NodeHandle nodeHandler_, nodePrivate_;
     geometry_msgs::TransformStamped transform_;
     tf2_ros::Buffer tfBuffer_;
     visualization_msgs::Marker dragoonMarker_;
-    std::string dragoonFrame_ = "dragoon_body";
+    std::string markerFrame_;
 
 
     public:
-        MarkerNode(){
+        MarkerNode() : nodePrivate_("~")
+        {
             // initialize the listener and the publisher
-            tf2_ros::TransformListener tfListener(tfBuffer_);
-            markerPub_ = nodeHandler_.advertise<visualization_msgs::Marker>("visual_marker", 0);
+            markerPub_ = nodeHandler_.advertise<visualization_msgs::Marker>("visualization_marker", 0);
+            nodePrivate_.param<std::string>("marker_frame", markerFrame_, "dragoon_body");
 
         }
 
         // main run tasks
         void run()
         {
-            while (nodeHandler_.ok())
-            {
-                try
-                {
-                    // listen for the dragoon transform
-                    transform_ = tfBuffer_.lookupTransform(dragoonFrame_, "world", ros::Time(0));
-                }
-                catch (tf2::TransformException &ex)
-                {
-                    ROS_WARN("%s", ex.what());
-                    ros::Duration(1.0).sleep();
-                    continue;
-                }
-            }
-
             constructMarker(transform_);
         }
 
         // this constructs the marker and publishes it
         void constructMarker(const geometry_msgs::TransformStamped &msg)
         {
-            dragoonMarker_.header.frame_id = dragoonFrame_;
+            dragoonMarker_.header.frame_id = markerFrame_;
             dragoonMarker_.header.stamp = ros::Time();
             dragoonMarker_.id = 0;
             dragoonMarker_.type = visualization_msgs::Marker::MESH_RESOURCE;
             dragoonMarker_.action = visualization_msgs::Marker::ADD;
-            dragoonMarker_.pose.position.x = transform_.transform.translation.x;
-            dragoonMarker_.pose.position.y = transform_.transform.translation.y;
-            dragoonMarker_.pose.position.z = transform_.transform.translation.z;
-            dragoonMarker_.pose.orientation.x = transform_.transform.rotation.x;
-            dragoonMarker_.pose.orientation.y = transform_.transform.rotation.y;
-            dragoonMarker_.pose.orientation.z = transform_.transform.rotation.z;
-            dragoonMarker_.pose.orientation.w = transform_.transform.rotation.w;
+            dragoonMarker_.pose.position.x = 0;
+            dragoonMarker_.pose.position.y = 0;
+            dragoonMarker_.pose.position.z = 0;
+            dragoonMarker_.pose.orientation.x = 0;
+            dragoonMarker_.pose.orientation.y = 0;
+            dragoonMarker_.pose.orientation.z = 0;
+            dragoonMarker_.pose.orientation.w = 1;
+            // dragoonMarker_.pose.position.x = transform_.transform.translation.x;
+            // dragoonMarker_.pose.position.y = transform_.transform.translation.y;
+            // dragoonMarker_.pose.position.z = transform_.transform.translation.z;
+            // dragoonMarker_.pose.orientation.x = transform_.transform.rotation.x;
+            // dragoonMarker_.pose.orientation.y = transform_.transform.rotation.y;
+            // dragoonMarker_.pose.orientation.z = transform_.transform.rotation.z;
+            // dragoonMarker_.pose.orientation.w = transform_.transform.rotation.w;
             dragoonMarker_.scale.x = 1.0;
             dragoonMarker_.scale.y = 1.0;
             dragoonMarker_.scale.z = 1.0;
             dragoonMarker_.color.a = 1.0; // Don't forget to set the alpha!
             dragoonMarker_.color.r = 1.0;
-            dragoonMarker_.color.g = 0.0;
-            dragoonMarker_.color.b = 0.0;
-            dragoonMarker_.mesh_resource = "package://meshes/dragoon_base.dae";
+            dragoonMarker_.color.g = 94/255;
+            dragoonMarker_.color.b = 19/255;
+            // use the dragoon mesh
+            dragoonMarker_.mesh_resource = "package://dragoon_simulation/meshes/dragoon_base.dae";
             markerPub_.publish(dragoonMarker_);
         }
 
 };
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "simple_node");
+  ros::init(argc, argv, "marker_node");
   MarkerNode node;
-  node.run();
+  
   while (ros::ok())
+    node.run();
     ros::spinOnce();
   return 0;
 
